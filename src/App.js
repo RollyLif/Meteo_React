6137eae378e80a04396bb03f05e48116ddd33c87
RollyLif <relifungula@gmail.com>
2022-08-26 08:51:30 +0200

    features: structure of application

--- src/App.js
+++ src/App.js
@@ -8,12 +8,23 @@ function App() {
     fetch("https://api.openweathermap.org/data/2.5/forecast?q="+data+"&appid=eac0fb388a3b92e0d649df28f5b0adb9&units=metric")
     .then(result => result.json())
     .then(result => console.log(result))
+    .catch(error => {
+      console.error('There was an error!', error);
+  });
   }); 
 
   return (
     <div className="App">
       <label for="fname">Ville:</label>
       <input type="text" id="fname" name="fname" value={data} onChange={(e) => {setData(e.target.value)}}/><br/>
+      <div className='card'>
+        <h2>Goma</h2>
+        <section className='date'>
+          <img src="" alt="img meteo"/>
+          <p>25/08/2022 18:00:00</p>
+        </section>
+        <h1>20°</h1>
+      </div>
     </div>
   );
 }
